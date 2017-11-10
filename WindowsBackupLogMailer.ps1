ef0e328e2070d89b80662f412a11d3517dac2f3c
Jefta Dirksen <j.dirksen@ridder.nl>
2017-11-10 15:08:13 +0100

    Added backupset

--- WindowsBackupLogMailer.ps1
+++ WindowsBackupLogMailer.ps1
@@ -1,16 +1,31 @@
 ﻿. "$PSScriptRoot\EmailCredentials.ps1"
 
+# Get Backupset
+$backupSet = "Files:`n"
+(Get-WBPolicy).FilesSpecsToBackup | ForEach-Object {
+    $backupSet += $_.FilePath + $_.FileName + "`n"
+}
+$backupSet += "VMs:`n"
+(Get-WBPolicy).ComponentsToBackup | ForEach-Object {
+    $backupSet += $_.VMName + "`n"
+}
+
+# Get Logs
 $logs = Get-WinEvent Microsoft-Windows-Backup |
 Where-Object { $_.TimeCreated -gt (Get-Date).AddDays(-1) } |
 Select-Object TimeCreated, Message |
-Sort-Object TimeCreated
+Sort-Object TimeCreated |
+Out-String
 
+# Setup E-mail body
+$body = "BackupSet:`n" + $backupSet + $logs
+
+# Send E-mail
 $EmailFrom = $Email
 $EmailTo = $Email
 $msg = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
 $msg.Subject = "WindowsBackupLog"
-$msg.IsBodyHtml = $true
-$msg.Body = $logs | ConvertTo-Html
+$msg.Body = $body
 $SMTPServer = "smtp.gmail.com"
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPClient.EnableSsl = $true
